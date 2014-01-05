f79c201fc17e27b71ce6d5cf2d6697db4b17f539
trieukhang <hoangtrieukhang@gmail.com>
2014-01-06 01:14:44 +0700

    test

---
+++ hook.sh
@@ -0,0 +1,24 @@
+# First, get the zip file
+cd .. && wget -O join-master.zip -q https://github.com/ToanNG/join/archive/master.zip
+
+#second, unzip it, if the zip file exists
+if [ -f join-master.zip ]; then
+    # Unzip the zip file
+    unzip -q join-master.zip
+
+    # Delete zip file
+    #rm ../projectmaster.zip
+
+    # Rename project directory to desired name
+    mv join-master join-news
+
+    # Delete current directory
+    rm -rf join
+
+    # Replace with new files
+    mv join-news join
+
+    # Perhaps call any other scripts you need to rebuild assets here
+    # or set owner/permissions
+    # or confirm that the old site was replaced correctly
+fi
