c98dd371623896a6479e2415aa67990950ba1e92
trieukhang <hoangtrieukhang@gmail.com>
2014-01-06 11:06:00 +0700

    auto deloy

--- hook.js
+++ hook.js
@@ -12,7 +12,8 @@ gith({
 
     		// var exec = require('child_process').execFile;
             // Exec a shell script
-            exec('hook.sh', function(error, stdout, stderr) {
+            exec('../home/user/join/hook.sh', function(error, stdout, stderr) {
+            		console.log(__filename);
             		console.log("error = " + error);
                     // Log success in some manner
                     console.log( 'exec complete' );
